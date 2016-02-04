7a6f2f539b0a55aaebc13d35d11c905c527f25bd
Aaron Arntz <aarntz@gmail.com>
2016-02-03 20:47:31 -0500

    adds type statements

--- setup.sh
+++ setup.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 cd ~
-#install git if not present
-echo "Installing git if uninstalled..."
+#Check for necessary installations
+echo "Checking for necessary installations..."
+type git >/dev/null 2>&1 || { echo >&2 "git is not installed.  Aborting."; exit 1; }
+type tmux >/dev/null 2>&1 || { echo >&2 "tmux is not installed.  Aborting."; exit 1; }
+type vim >/dev/null 2>&1 || { echo >&2 "vim is not installed.  Aborting."; exit 1; }
 
 #Clone dotfiles individually
 echo "Cloning arntzy's dotfiles to home directory..."
@@ -16,3 +19,7 @@ echo "Creating symlinks to dotfiles..."
 ln -s .vim/.vimrc .vimrc
 ln -s .tmux/.tmux.conf .tmux.conf
 ln -s .tmux/.tmux.conf.local .tmux.conf.local
+
+#Potentially useful leftover files
+#command -v foo >/dev/null 2>&1 || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
+#hash foo 2>/dev/null || { echo >&2 "I require foo but it's not installed.  Aborting."; exit 1; }
