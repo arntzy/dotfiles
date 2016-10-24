cdf2f94a25d954d07ca034739e62a9a870d60f79
Aaron Arntz <aarntz@gmail.com>
2016-10-24 12:27:26 -0400

    add pi .vim configuration

--- pi_setup.sh
+++ pi_setup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 cd ~
+
+#Install Node
+wget https://nodejs.org/dist/latest-v6.x/node-v6.9.1-linux-armv7l.tar.gz
+tar -xzf node-v6.9.1-linux-armv7l.tar.gz
+cd node-v6.9.1-linux-armv7l
+sudo cp -R * /usr/local/
+
 #Check for necessary installations
 echo "Checking for necessary installations..."
 type git >/dev/null 2>&1 || { echo >&2 "git is not installed.  Aborting."; exit 1; }
@@ -8,9 +15,9 @@ type vim >/dev/null 2>&1 || { echo >&2 "vim is not installed.  Aborting."; exit 
 
 #Clone dotfiles individually
 echo "Cloning arntzy's dotfiles to home directory..."
-echo "Cloning .vim/..."
+echo "Cloning arntzy's .vim/..."
 git clone --recursive https://github.com/arntzy/.vim.git
-echo "Cloning git/..."
+echo "Cloning arntzy's git/..."
 git clone https://github.com/arntzy/git.git
 
 #Create symlinks to dotfiles
@@ -19,3 +26,11 @@ ln -sf .vim/.vimrc .vimrc
 ln -sf git/.gitconfig ~
 ln -sf git/.gitignore_global ~
 ln -sf dotfiles/.bashrc .bashrc
+
+#Cleanup
+echo "Removing Oblique vim plugin..."
+rm -rf ~/.vim/bundle/oblique/
+echo "Removing Ultisnips vim plugin..."
+rm -rf ~/.vim/bundle/ultisnips/
+echo "Commenting out colorscheme in .vimrc..."
+sed -i '18 s/^/"/' ~/.vimrc
